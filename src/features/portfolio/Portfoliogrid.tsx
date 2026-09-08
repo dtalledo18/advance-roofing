7ab79c7d796b17381cfc03c8b6ebc87c960c84d9
dtalledo18 <dtalledo@advancedteamelite.com>
2026-09-08 11:54:23 -0500

    add: no border radious

--- src/features/portfolio/Portfoliogrid.tsx
+++ src/features/portfolio/Portfoliogrid.tsx
@@ -69,14 +69,7 @@ export function PortfolioGrid({ projects }: PortfolioGridProps) {
                                     className={styles.image}
                                     sizes="(max-width: 768px) 100vw, (max-width: 1280px) 50vw, 33vw"
                                 />
-                                {/* Badge categoría */}
-                                <span className={`${styles.categoryBadge} ${
-                                    project.category === 'Commercial'
-                                        ? styles.badgeCommercial
-                                        : styles.badgeResidential
-                                }`}>
-                                    {project.category}
-                                </span>
+
                             </div>
 
                             {/* Contenido */}
